f2aacedb9c557d2ad9b57e84cd692932a6e87ddb
Fernanda Vidal <fevidal.dev@gmail.com>
2022-03-22 19:25:35 -0300

    a

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 const sectionInput = document.querySelector('#interage');
-const InputBox = document.createElement('input');
-InputBox.id = 'texto-tarefa';
+const inputBox = document.createElement('input');
+inputBox.id = 'texto-tarefa';
 
-sectionInput.appendChild(InputBox);
+sectionInput.appendChild(inputBox);
+
+const buttonTask = document.createElement('button');
+buttonTask.id = 'criar-tarefa';
+
+
+sectionInput.appendChild(buttonTask);
+
+
+const sectionOl = document.getElementById('lista-tarefas');
+const botao = document.getElementById('criar-tarefa');
+
+function addTask(){
+let lista = document.createElement('li'); 
+let textoInput = document.getElementById('texto-tarefa');
+
+lista.innerText = textoInput.value;
+if (lista.innerText === ''){
+    alert('adicione um ítem');
+} else{
+sectionOl.appendChild(lista);
+}
+
+document.getElementById('texto-tarefa').value = '';
+
+}
+addTask()
+
+botao.addEventListener('click', addTask);
